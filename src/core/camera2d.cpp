ea6f17301bd573e32ddd34b360b747f90d039b1c
Vinicius Lobo <viniciuslobotrust@gmail.com>
2025-07-30 20:47:27 -0300

    impr: A camera agora retorna a posição do mouse de acordo com o espaco 2D

--- src/core/camera2d.cpp
+++ src/core/camera2d.cpp
@@ -13,3 +13,38 @@ void Camera2D::UpdateView() {
 mat4 Camera2D::GetViewProjection() const {
     return projection * view;
 }
+
+vec2 Camera2D::ScreenToWorld(int mouseX, int mouseY) const
+{
+    // Proporção da tela
+    float aspect = (float)screenWidth / (float)screenHeight;
+
+    // Coordenadas normalizadas da tela (0 a 1)
+    float normX = (float)mouseX / (float)screenWidth;
+    float normY = (float)mouseY / (float)screenHeight;
+
+    // Coordenadas do mundo (com ajuste de espaço e proporção)
+    float worldX, worldY;
+
+    if (screenWidth >= screenHeight) {
+        // Horizontal dominante
+        float viewWidth = space * 2.0f;
+        float viewHeight = viewWidth * (float)screenHeight / (float)screenWidth;
+
+        worldX = (normX * viewWidth) - space;
+        worldY = (1.0f - normY) * viewHeight - (viewHeight / 2.0f);
+    } else {
+        // Vertical dominante
+        float viewHeight = space * 2.0f;
+        float viewWidth = viewHeight * (float)screenWidth / (float)screenHeight;
+
+        worldX = (normX * viewWidth) - (viewWidth / 2.0f);
+        worldY = (1.0f - normY) * viewHeight - space;
+    }
+
+    // Adiciona a posição da câmera
+    worldX += position.x;
+    worldY += position.y;
+
+    return vec2(worldX, worldY);
+}
--- src/core/camera2d.h
+++ src/core/camera2d.h
@@ -7,8 +7,11 @@ using namespace maths;
 class Camera2D {
 public:
     float space = 10.0f;
-    int screenWidth = 800;
-    int screenHeight = 600;
+
+    int &screenWidth;
+    int &screenHeight;
+
+    inline Camera2D(int &swidth, int &sheight): screenWidth(swidth), screenHeight(sheight) {}
 
     mat4 projection;
     mat4 view;
@@ -18,4 +21,5 @@ public:
     void Resize(int width, int height);
     void UpdateView();
     mat4 GetViewProjection() const;
+    vec2 ScreenToWorld(int mouseX, int mouseY) const;
 };
