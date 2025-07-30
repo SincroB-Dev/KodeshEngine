9e13bfbc7990f40ccf2cfbc4ce4e6865549f6915
Vinicius Lobo <viniciuslobotrust@gmail.com>
2025-07-29 22:39:01 -0300

    impr: Aplicação da cor com a classe Color

--- src/maths/color.h
+++ src/maths/color.h
@@ -11,10 +11,11 @@ struct Color
     float a=1.0f;
 
     Color() = default;
-    Color(float r, float g, float b, float a = 1.0f)
-        : r(r), g(g), b(b), a(a) {}
+    Color(float r, float g, float b, float a = 1.0f);
 
-    friend std::ostream& operator<<(std::ostream &os, const Color &c)
+    void apply();
+
+    inline friend std::ostream& operator<<(std::ostream &os, const Color &c)
     {
         os << "red: " << c.r << ", green: " << c.g << ", blue: " << c.b << ", alpha:" << c.a;
         return os;
