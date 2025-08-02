839ed70389450d0049b96fc29b9a1a548b25332d
Vinicius Lobo <viniciuslobotrust@gmail.com>
2025-08-01 22:52:56 -0300

    lil-fix: Pequenos ajustes no painel dockado

--- src/ui/dockedToolbox.h
+++ src/ui/dockedToolbox.h
@@ -4,11 +4,33 @@ namespace core
 {
     namespace ui
     {
+        enum class TBEditorTab
+        {
+            Ambiente,
+            Cenario,
+            Entidade,
+            Material,
+            Componentes
+        };
+
         class DockedToolbox
         {
-        public:
-            inline DockedToolbox() {}
+            TBEditorTab activeTab = TBEditorTab::Ambiente;
 
+            float panelWidth = 300.0f;
+
+            const float panelWidthMin = 200.0f;
+            const float panelWidthMax = 500.0f;
+
+            const float iconButtonSize;
+            const float tabColumnWidth;
+
+        public:
+            inline DockedToolbox(float wmin = 200.0f, float wmax = 500.0f, float iconbsize = 30.0f, float tabcolwidth = 4.0f)
+                : panelWidthMin(wmin), panelWidthMax(wmax), iconButtonSize(iconbsize), tabColumnWidth(iconbsize + tabcolwidth)
+            {}
+
+            void DrawIconButton(const char *icon, TBEditorTab tab);
             void Draw();
         };
     };
