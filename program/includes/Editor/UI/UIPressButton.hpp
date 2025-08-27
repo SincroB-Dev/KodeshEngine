0252fef70b463b8835133354e19c1603d5363ad8
Vinicius Lobo <viniciuslobotrust@gmail.com>
2025-08-27 01:27:49 -0300

    feat: Botão que captura entradas do usuário para uso posterior

---
+++ program/includes/Editor/UI/UIPressButton.hpp
@@ -0,0 +1,99 @@
+#include "Editor/UI/UIComponent.hpp"
+#include "Core/Utils/UniqueIDGen.hpp"
+
+#include <functional>
+
+namespace editor::ui
+{
+	/**
+	 * @brief Deixa o tipo de UIPressButton mais genérico, facilitando a chamada.
+	 **/
+	enum class UIPressButtonType
+	{
+		Keyboard, 
+		MouseButton, 
+		JoyButton
+	};
+
+	/**
+	 * @brief Componente de interface responsável por aguardar a captura de um evento (Teclado/Mouse)
+	 *        de entrada do usuário e armazena-lo para uso posterior.
+	 **/
+	class UIPressButton : public UIComponent
+	{
+	public:
+		UIPressButton(UIPressButtonType type,
+					  std::string emptyLabel, std::string awaitingLabel,
+					  int defaultCode = -1)
+			: m_EmptyLabel(emptyLabel), m_AwaitingLabel(awaitingLabel),
+			  m_Type(type), m_IsAwaiting(false)
+		{
+			if (defaultCode > 0)
+			{
+				// Necessita ser chamado para a inclusão do título de captura.
+				SetCaptured(defaultCode);
+			}
+		}
+
+		~UIPressButton()
+		{
+			// Impede um dangling pointer (ponteiro pendente)
+			if (this == s_CurrentAwaiting)
+			{
+				ResetButtonState();
+			}
+		}
+
+		/**
+		 * @brief Retorna o código de acionamento do evento. 
+		 **/
+		int GetCode() const { return m_Code; }
+
+		/**
+		 * @brief Retorna o título de captura do evento.
+		 **/
+		const char* GetLabel() const { return GetCode() > 0 ? m_Captured.c_str() : "<Empty>"; }
+
+		/**
+		 * @brief Retorna se o botão está aguardando alguma entrada.
+		 **/
+		bool const IsAwaiting() const { return m_IsAwaiting; }
+
+		/**
+		 * @brief Seta a tecla capturada, usada até mesmo para restaurar o estado salvo do botão. 
+		 **/
+		void SetCaptured(int code = -1);
+		
+		/**
+		 * @brief Faz um reset em propriedades essenciais do botão, sendo m_IsAwaiting, m_Code, 
+		 * 		  e também reseta a singletron s_CurrentAwaiting.
+		 **/
+		void ResetButtonState();
+
+		/**
+		 * @brief Renderiza o botão, quando m_IsAwaiting está true, ele vai permanecer com
+		 * 		  cor de ativo do botão.
+		 **/
+		virtual void Render() override;
+
+		/**
+		 * @brief Comportamento do botão em estado ativo, ou seja, m_IsAwaiting true.
+		 *        Aqui ele aguarda uma entrada de botão/tecla efetuada pelo usuário.
+		 **/
+		void HandlePressButton();
+
+
+	private:
+		std::string m_EmptyLabel; // Texto do botão quando está sem captura
+		std::string m_AwaitingLabel; // Texto do botão quando está esperando por uma captura
+		std::string m_Captured; // Texto informativo do botão que mostra a tecla, botão do mouse, joy... Que foi capturado
+
+		UIPressButtonType m_Type; // Serve como filtro de que tipo de entrada o botão aceita. ( Aqui não é a casa da mãe joana... )
+
+		bool m_IsAwaiting; // Indica quando o botão está aguardando para fazer uma captura.
+		int m_Code; // Código capturado.
+
+		// Usado para evitar que mais de um botão entre no estado de espera de um key code.
+		static UIPressButton* s_CurrentAwaiting;
+	};
+}
---
+++ program/source/Editor/UI/UIPressButton.cpp
@@ -0,0 +1,101 @@
+#include "Editor/UI/UIPressButton.hpp"
+#include <imgui/imgui.h>
+
+namespace editor::ui
+{
+	// Inicialização do static que verifica se tem 1 botão esperando input
+	UIPressButton* UIPressButton::s_CurrentAwaiting = nullptr;
+
+	void UIPressButton::HandlePressButton()
+	{
+		if (m_IsAwaiting)
+		{
+			for (int key = ImGuiKey_NamedKey_BEGIN; key < ImGuiKey_NamedKey_END; key++)
+			{
+			    ImGuiKey imguiKey = (ImGuiKey)key;
+
+			    if (ImGui::IsKeyPressed(imguiKey))
+			    {
+			    	if ((m_Type == UIPressButtonType::Keyboard && key >= ImGuiKey_MouseLeft) ||
+			    		(m_Type == UIPressButtonType::MouseButton && key < ImGuiKey_MouseLeft))
+			    	{
+			    		SetCaptured(-1);
+			    		break;
+			    	}
+
+			        SetCaptured(key);
+                    break;
+			    }
+			}
+		}
+	}
+
+	void UIPressButton::SetCaptured(int code)
+	{
+        m_Captured = code > 0 ? ImGui::GetKeyName((ImGuiKey)code) : "<Empty>";
+        
+        m_Code = code;
+        m_IsAwaiting = false;
+
+        s_CurrentAwaiting = nullptr;
+	}
+
+	void UIPressButton::ResetButtonState()
+	{
+		m_IsAwaiting = false;
+		s_CurrentAwaiting->m_Code = -1;
+		s_CurrentAwaiting = nullptr;
+	}
+
+	void UIPressButton::Render()
+	{
+		std::string label = m_IsAwaiting
+							? m_AwaitingLabel
+							: m_Code >=0
+								? m_Captured
+								: m_EmptyLabel;
+
+		bool isPushStyleColor = false;
+
+		// Caso o botão esteja aguardando algum evento, ele é travado na cor de active
+		if (m_IsAwaiting)
+		{
+			ImVec4 newBtnColor = ImGui::GetStyleColorVec4(ImGuiCol_ButtonActive);
+
+			ImGui::PushStyleColor(ImGuiCol_Button, newBtnColor);
+			ImGui::PushStyleColor(ImGuiCol_ButtonHovered, newBtnColor);
+			ImGui::PushStyleColor(ImGuiCol_ButtonActive, newBtnColor);
+
+			isPushStyleColor = true;
+		}
+
+		// Identificador único do botão, evitando o uso do UniqueID para componentes de interface
+		ImGui::PushID(this);
+		{
+			if (ImGui::Button(label.c_str(), ImVec2(80, 25)) && !m_IsAwaiting)
+			{
+				if (s_CurrentAwaiting == nullptr)
+				{
+					m_IsAwaiting = true;
+					s_CurrentAwaiting = this;
+				}
+				else
+				{
+					if (this != s_CurrentAwaiting)
+					{
+						ResetButtonState();
+					}
+				}
+			}
+		}
+		ImGui::PopID();
+
+		// Remove o estilo do botão, solução per-frame
+		if (isPushStyleColor)
+		{
+			ImGui::PopStyleColor(3);
+		}
+
+		HandlePressButton();
+	}
+}
