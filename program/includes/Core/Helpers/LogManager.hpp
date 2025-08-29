81118c50ca30b81e5d9c1743905619af04730c96
Vinicius Lobo <viniciuslobotrust@gmail.com>
2025-08-29 15:40:27 -0300

    refactor: adicionados métodos para criar/destruir central de logs

--- program/includes/Core/Helpers/LogManager.hpp
+++ program/includes/Core/Helpers/LogManager.hpp
@@ -4,59 +4,111 @@
 #include <iostream>
 #include <vector>
 
-namespace core
+namespace core::systems
 {
-	namespace systems
-	{
-		// Tipos de log, para filtragem
-        enum class LogType { 
-        	Info, 
-        	Warning,
-        	Error,
-        	EDebug // Debug da engine, mensagens de inicialização e etc...
-        };
+	/**
+     * @brief Tipos de log, para filtragem
+     **/
+    enum class LogType { 
+    	Info, 
+    	Warning,
+    	Error,
+    	EDebug // Debug da engine, mensagens de inicialização e etc...
+    };
 
-        // Abstração de entrada do log
-        struct LogEntry 
-        {
-            std::string Message;
-            LogType Type;
-        };
+    /**
+     * @brief Abstração de entrada do log
+     **/
+    struct LogEntry 
+    {
+        std::string Message;
+        LogType Type;
+    };
 
-        class LogManager
-        {
-        public:
-            LogManager() {}
+    class LogManager
+    {
+        /**
+         * @brief Instancia privada, para que apenas a função create possa inicializar o logger.
+         **/
+        LogManager(unsigned int limit): m_LogsLimit(limit) {}
 
-        	// Permite o envio de logs de qualquer parte do sistema
-        	static LogManager* Main;
-            
-            // Insere um log na listagem
-            void PushLog(LogType type, const std::string& msg);
+        /**
+    	 * @brief Singletron responesável por administrar os logs, permitido apenas 1 instâcia.
+         **/
+    	static LogManager* Main;
 
-            // Limpa a listagem de logs
-            void Clear();
+        /**
+         * @brief Insere um log na listagem.
+         **/
+        void PushLog(LogType type, const std::string& msg);
 
-            // Chamada para setar o main para acesso do log em qualquer parte do sistema
-            void Instantiate();
-            
-            template<typename... Args>
-            static void Log(LogType type, Args&... msgs);
+    public:
+        /**
+         * @brief Limpa a listagem de logs
+         **/
+        static void Clear();
 
-            // Captura de listagem de logs do Main
-            static std::vector<LogEntry>& GetLogs();
+        /**
+         * @brief Chamada para setar o main para acesso do log em qualquer parte do sistema.
+         **/
+        static void Create(unsigned int limit = 1000);
 
-        private:
-        	std::vector<LogEntry> m_Logs;    	
-        };
+        /**
+         * @brief Destrói o logger aberto, lembrando que o logger é um singletron.
+         **/
+        static void Destroy();
 
+        /**
+         * @brief Verifica se há logger instanciado.
+         **/
+        static bool HasLogger();
+        
+        /**
+         * @brief Efetua um log na instancia singletron.
+         **/
         template<typename... Args>
-        void LogManager::Log(LogType type, Args&... msgs)
+        static void Log(LogType type, Args&... msgs);
+
+        /**
+         * @brief Captura de listagem de logs do Main.
+         **/
+        static std::vector<LogEntry>& GetLogs();
+
+    private:
+    	std::vector<LogEntry> m_Logs;
+
+        unsigned int m_LogsLimit;	
+    };
+
+    template<typename... Args>
+    void LogManager::Log(LogType type, Args&... msg)
+    {
+        if (Main != nullptr)
         {
-            if (Main != nullptr)
-            {
-                Main->PushLog((std::string(msgs) + ...));
-            }
+            Main->PushLog(type, (std::string(msg) + ...));
         }
-	}
+        else
+        {
+            static bool notifyAboutInstance = true;
+
+            // Aviso único de que o logger não apresentará notificações na interface.
+            if (notifyAboutInstance)
+            {
+                std::cout << "[Kernel] " << "Logger não encontrado, logs serão despachados no terminal." << std::endl;
+
+                notifyAboutInstance = false;
+            }
+
+            // Essa fun
+            static auto si_GetType = [&type]() {
+                if (type == LogType::Info) return "Info";
+                else if (type == LogType::Warning) return "Warning";
+                else if (type == LogType::Error) return "Error";
+                else if (type == LogType::EDebug) return "Kernel";
+                else return "Unknown";
+            };
+
+            std::cout << "[" << si_GetType() << "] " << (std::string(msg) + ...) << std::endl; 
+        }
+    }
 }
--- program/source/Core/Helpers/LogManager.cpp
+++ program/source/Core/Helpers/LogManager.cpp
@@ -1,37 +1,55 @@
 #include "Core/Helpers/LogManager.hpp"
+#include <stdexcept>
 
-namespace core
+namespace core::systems
 {
-	namespace systems
-	{
-		LogManager* LogManager::Main = nullptr;
+	LogManager* LogManager::Main = nullptr;
 
-		// Inclui um log na listagem, limitando a 1000 logs.
-        void LogManager::PushLog(LogType type, const std::string& msg)
+    void LogManager::PushLog(LogType type, const std::string& msg)
+    {
+        m_Logs.push_back({ msg, type });
+        if (m_Logs.size() > m_LogsLimit) m_Logs.erase(m_Logs.begin());
+    }
+
+    void LogManager::Clear()
+    {
+        if (Main != nullptr)
         {
-            m_Logs.push_back({ msg, type });
-            if (m_Logs.size() > 1000) m_Logs.erase(m_Logs.begin());
+    	   Main->m_Logs.clear();
+        }
+    }
+
+    std::vector<LogEntry>& LogManager::GetLogs()
+    {
+    	return Main->m_Logs;
+    }
+
+    void LogManager::Create(unsigned int limit)
+    {
+    	if (Main == nullptr)
+    	{
+    		Main = new LogManager(limit);
+    		return;
+    	}
+
+    	throw std::runtime_error("Não é possível instânciar mais de um Logger!");
+    }
+
+    void LogManager::Destroy()
+    {
+        if (Main != nullptr)
+        {
+            delete Main;
+            Main = nullptr;
+            std::cout << "[Kernel] " << "Instância principal de Logger destruída." << std::endl;
+            return;
         }
 
-        void LogManager::Clear()
-        {
-        	m_Logs.clear();
-        }
+        throw std::runtime_error("Nenhuma instância de Logger foi construída!");
+    }
 
-        std::vector<LogEntry>& LogManager::GetLogs()
-        {
-        	return Main->m_Logs;
-        }
-
-        void LogManager::Instantiate()
-        {
-        	if (Main == nullptr)
-        	{
-        		Main = this;
-        		return;
-        	}
-
-        	std::runtime_error("Não é possível instanciar mais de um LogManager");
-        }
-	}
+    bool LogManager::HasLogger()
+    {
+        return Main != nullptr;
+    }
 }
