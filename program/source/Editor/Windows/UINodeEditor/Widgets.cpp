681f362bde52a13c129c63d6ef2db1bca1c5c51f
Vinicius Lobo <viniciuslobotrust@gmail.com>
2025-08-30 20:17:11 -0300

    feat: popups para o UINodeEditor

--- program/source/Editor/Windows/UINodeEditor/Widgets.cpp
+++ program/source/Editor/Windows/UINodeEditor/Widgets.cpp
@@ -1,10 +1,11 @@
 #include "Editor/Windows/UINodeEditor/Widgets.hpp"
 #include "Core/Maths/ImGuiOperators.hpp"
 
+#include <imgui/imgui.h>
 #include <imgui/imgui_internal.h>
 
 namespace editor::nodes::widgets
-{ 
+{
 	void SocketIcon(const ImVec2& size, IconType type, bool filled, const ImVec4& color, const ImVec4& innerColor)
 	{
 	    if (ImGui::IsRectVisible(size))
@@ -16,5 +17,42 @@ namespace editor::nodes::widgets
 	    }
 
 	    ImGui::Dummy(size);
+	}
+
+	//------------------------------------
+	// Popup
+	//------------------------------------
+
+	void UINodePopup::OpenPopup(UINodePopupFn fn, int popupId)
+	{
+		s_PopupId = popupId;
+		s_PopupFn = std::move(fn);
+	}
+
+	void UINodePopup::Close()
+	{
+		ImGui::CloseCurrentPopup(); 
+
+		// Limpa a função de popup
+		s_PopupFn = nullptr;
+	}
+
+	void UINodePopup::Render()
+	{
+		if (s_PopupId != -1)
+		{
+		    ImGui::OpenPopup("NodeEditorPopup");
+		    s_PopupId = -1; // evita reabrir no próximo frame
+		}
+
+		if (ImGui::BeginPopup("NodeEditorPopup")) 
+		{
+		    if (s_PopupFn) 
+    		{
+		    	s_PopupFn();
+    		}
+
+		    ImGui::EndPopup();
+		}
 	}
 }
