ef54ad726abfd679d07f20e7b51ca76bda7631ec
Vinicius Lobo <viniciuslobotrust@gmail.com>
2025-09-07 12:14:44 -0300

    feature: adicionado metodos para deserialização de sistemas

--- program/includes/Core/Serialization/PersistenceRegistry.hpp
+++ program/includes/Core/Serialization/PersistenceRegistry.hpp
@@ -7,6 +7,9 @@
 
 #include "Core/ECS/Entity.hpp"
 #include "Core/ECS/EntityRegistry.hpp"
+#include "Core/Input/InputManager.hpp"
+#include "Core/Events/EventDispatcher.hpp"
+#include "Core/Systems/ISystem.hpp"
 
 #include "Core/Serialization/Persistence/JsonComponents.hpp"
 
@@ -22,7 +25,18 @@ namespace core::serialization
 	/**
 	 * @brief Recebe entity, registry e json com dados. 
 	 **/
-	using ComponentDeserializeFn = std::function<void(const ecs::Entity&, ecs::EntityRegistry&, nlohmann::json&)>;
+	using ComponentDeserializeFn = std::function<void(const ecs::Entity&, ecs::EntityRegistry&, const nlohmann::json&)>;
+
+	/**
+	 * @brief Utilização específica para deserializar sistemas.
+	 **/
+	using DeserializeSystemFn = std::function<std::unique_ptr
+		<systems::ISystem> (
+			events::EventDispatcher&,
+			input::InputManager&,
+			const nlohmann::json&
+		)
+	>;
 
 	/**
 	 * Entrada de serializadores.
@@ -52,6 +66,11 @@ namespace core::serialization
 			// Reserva 8 slots para entrada de componentes, futuramente aumentar para diminuir interações,
 			// considerando que o uso de vectors está cada vez maior no software.
 			csr.m_ComponentSerializers.reserve(8);
+
+			// Reserva 4 slots para entrada de sistemas, o software ainda conta com dois sistemas,
+			// porém, futuramente se encaixarão outros como AudioManager, e PhysicsManager.
+			// csr.m_SystemsDeserializers.reserve(4);
+
 			return csr;
 		}
 
@@ -64,6 +83,12 @@ namespace core::serialization
 		void RegisterComponent(const std::string& name);
 
 		/**
+		 * @brief Método para registro de deserializador de sistemas, lembrando que é 1 registro por sistema.
+		 **/
+		template<typename T, typename... Args>
+		void RegisterSystem(const std::string& name, Args&&... args);
+
+		/**
 		 * @brief Armazena componentes serializados em um json object.
 		 **/
 		void SerializeComponents(const ecs::Entity& entity, ecs::EntityRegistry& registry, nlohmann::json& jsonComponents);
@@ -71,13 +96,25 @@ namespace core::serialization
 		/**
 		 * @brief Deserializa json objects em componentes, e os linka diretamente ao entity.
 		 **/
-		void DeserializeComponents(const ecs::Entity& entity, ecs::EntityRegistry& registry, nlohmann::json& jsonComponents);
+		void DeserializeComponents(const ecs::Entity& entity, ecs::EntityRegistry& registry, const nlohmann::json& jsonComponents);
+
+		/**
+		 * @brief Deserializa json objects em sistemas, em uma listagem. 
+		 **/
+		std::unique_ptr<systems::ISystem> DeserializeSystem(const std::string& name, events::EventDispatcher& dispatcher, input::InputManager& input, const nlohmann::json& j);
 
 	private:
 		/**
 		 * @brief Armazena entradas de serialização/deserialização de componentes ECS. 
 		 **/
 		std::vector<ComponentSerializerEntry> m_ComponentSerializers;
+
+		/**
+		 * @brief Armazena os deserializadores dos sistemas, todo sistema tem um deserializador,
+		 *        nem todo deserializador é obrigado a ser utilizado, todo sistema pode ser salvo
+		 *        /ou não, porém, todo sistema salvo deve ser carregado!
+		 **/
+		std::unordered_map<std::string, DeserializeSystemFn> m_SystemsDeserializers;
 	};
 
 	template<typename T>
@@ -94,12 +131,34 @@ namespace core::serialization
 	        },
 
 	        // Deserialização
-	        [](const ecs::Entity& entity, ecs::EntityRegistry& registry, nlohmann::json& data)
+	        [](const ecs::Entity& entity, ecs::EntityRegistry& registry, const nlohmann::json& data)
 	    	{
 	            registry.AddComponent<T>(entity, persistence::DeserializeComponent<T>(data));
 	        }
 		};
 
     	m_ComponentSerializers.push_back(std::move(Entry));
+	}
+
+	template<typename T, typename... Args>
+	void PersistenceRegistry::RegisterSystem(const std::string& name, Args&&... args)
+	{
+	    m_SystemsDeserializers[name] =
+	        [targs = std::make_tuple(std::forward<Args>(args)...)]
+	        (events::EventDispatcher& dispatcher, input::InputManager& input, const nlohmann::json &j) -> std::unique_ptr<systems::ISystem>
+		    {
+		    	return std::apply(
+	                [&](auto&&... unpacked)
+			        {
+			            // Cria o sistema
+			            auto sys = std::make_unique<T>(dispatcher, input, std::forward<decltype(unpacked)>(unpacked)...);
+
+			            // Utiliza a função especializada para carregar os dados.
+			            persistence::DeserializeSystem<T>(sys.get(), j);
+
+			            return sys;
+			        }, targs
+		        );
+		    };
 	}
 }
